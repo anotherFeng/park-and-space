edce574af731bbb3c2f7cd68b5805e9aec5493b0
anotherFeng <regnagleppod@gmail.com>
2018-08-04 00:42:27 -0700

    finish server side and database setup

---
+++ database/index.js
@@ -0,0 +1,22 @@
+// const mongoose = require('mongoose');
+// const dotenv = require('dotenv').config();
+// const Rental = require('./models/rental');
+// const FakeDb = require('./fake-db');
+
+// const MONGO_URI = `mongodb://${process.env.MLAB_USER}:${process.env.MLAB_PW}ds111562.mlab.com:11562/ng-bnb`;
+// if(!MONGO_URI){
+//   throw new Error('Please provide a valid mLab URI');
+// }
+
+// mongoose.Promise = global.Promise;
+// mongoose.connect(MONGO_URI, { useNewUrlParser: true })
+//   .then(() => {
+//     const fakeDb = new FakeDb();
+//     fakeDb.seeDb();
+//   });
+
+// const db = mongoose.connection;
+// db.once('open', () => console.log('Connected to MongoLab instance.'))
+//   .on('error', error => console.log('Error connecting to MongoLab:', error));
+
+// module.exports = db;
---
+++ server/controllers/rentalControllers.js
@@ -0,0 +1,22 @@
+const Rental = require('../../database/models/rental');
+
+exports.getRentals = (req, res) => {
+  Rental.find({})
+    .then((found) => {
+      res.json(found);
+    })
+    .catch((err) => {
+      res.status(404).send(err);
+    })
+}
+
+exports.getRentalById = (req, res) => {
+  const rentalId = req.params.id;
+  Rental.findById(rentalId)
+    .then((found) => {
+      res.json(found);
+    })
+    .catch((err) => {
+      res.status(422).send(err)
+    })
+}
