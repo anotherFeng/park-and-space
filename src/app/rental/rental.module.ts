ac5ae0a6347029d46e5c6db1992a85915f4ae276
anotherFeng <regnagleppod@gmail.com>
2018-08-04 20:10:46 -0700

    improve rental detail by using pipes and font-awesome to improve text

--- src/app/rental/rental.module.ts
+++ src/app/rental/rental.module.ts
@@ -10,6 +10,8 @@ import { RentalComponent } from './rental.component';
 import { RentalService } from './shared/rental.service';
 import { RentalDetailComponent } from './rental-detail/rental-detail.component';
 
+import { UppercasePipe } from '../shared/pipes/uppercase.pipe';
+
 const routes: Routes = [
   {path: 'rentals', component: RentalComponent, 
     children: [
@@ -24,11 +26,12 @@ const routes: Routes = [
     RentalListEntryComponent,
     RentalComponent,
     RentalDetailComponent,
+    UppercasePipe
   ],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [
     RentalService
