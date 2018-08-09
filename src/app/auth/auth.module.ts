0003a5faca6283a0b201215ebe8d8ef17f0367ad
anotherFeng <regnagleppod@gmail.com>
2018-08-08 23:15:41 -0700

    add validation to register fields

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
@@ -17,7 +18,8 @@ const routes: Routes = [
   ],
   imports: [
     RouterModule.forRoot(routes),
-    FormsModule
+    FormsModule,
+    CommonModule
   ],
   providers: [],
 })
