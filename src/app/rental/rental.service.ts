82231babb24c478f45f00e9ee9ee70aa6d071e30
anotherFeng <regnagleppod@gmail.com>
2018-08-20 00:41:53 -0700

    add service to fetch rentals based on city, add rental-search component

--- src/app/rental/rental.service.ts
+++ src/app/rental/rental.service.ts
@@ -10,9 +10,13 @@ export class RentalService {
 
   public getRentalById(rentalId: string): Observable<any> {
     return this.http.get(`/api/v1/rentals/${rentalId}`)
-  }
+  };
 
   public getRentals(): Observable<any> {
     return this.http.get('/api/v1/rentals');
+  };
+
+  public getRentalsByCity(city: string): Observable<any> {
+    return this.http.get(`/api/v1/rentals?city=${city}`);
   }
-}
+};
